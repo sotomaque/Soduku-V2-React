010a14eb898bf4c0ff37a8edfe56ff380107f450
sotomaque <sotomaque@gmail.com>
2020-05-06 02:00:27 -0700

    created actions, dispatched createGrid action, mapped data to cells to be displayed in block component

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,14 +1,20 @@
-import React, { Children, FC } from 'react';
+import React, { Children, FC, useCallback, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { AnyAction, Dispatch } from 'redux';
 
-import { createFullGrid } from 'utils';
+import { createGrid } from 'reducers';
 
 import Block from './block';
 import { Container, Row } from './styles';
 
 const Grid: FC = () => {
-  
-  const grid = createFullGrid();
-  console.log(grid);
+  const dispatch = useDispatch<Dispatch<AnyAction>>();
+
+  const create = useCallback(() => dispatch(createGrid()), [dispatch]);
+
+  useEffect(() => {
+    create();
+  }, [create])
 
   return (
     <Container data-cy="grid-container">
---
+++ src/reducers/actions.ts
@@ -0,0 +1,7 @@
+import { Action } from 'redux';
+
+import * as types from './types';
+
+export const createGrid = (): Action => ({
+    type: types.CREATE_GRID
+});
