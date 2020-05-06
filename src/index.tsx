85472d5a477cafa4230e218fbb85f40edcf70091
sotomaque <sotomaque@gmail.com>
2020-05-05 21:57:57 -0700

    set up themeing with styled components; set up declaration for theme, implemented theme in index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 
 import * as serviceWorker from './serviceWorker';
-import { GlobalStyles } from './styles';
+import { GlobalStyles, theme } from './styles';
 
 ReactDOM.render(
-  <>
+  <ThemeProvider theme={theme}>
     <GlobalStyles />
     <div>Hi</div>
-  </>,
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,7 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 
 export default createGlobalStyle`
+${({ theme }) => css`
     html {
         height: 100%;
 
@@ -11,8 +12,8 @@ export default createGlobalStyle`
             margin: 0;
 
             #root {
-                background: radial-gradient(#282c34cc, #282c34);
-                color: '#282c34';
+                background: ${theme.colors.background};
+                color: ${theme.colors.black};
                 display: flex;
                 font-family: sans-serif;
                 height: 100%;
@@ -21,4 +22,5 @@ export default createGlobalStyle`
             }
         }
     }
+`}
 `
