11c125ac895abf57e353eb3eb9dfc62931c96b2e
sotomaque <sotomaque@gmail.com>
2020-05-05 22:17:08 -0700

    updated ts to fix known bug

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 
+import { Content, Title } from './components';
 import * as serviceWorker from './serviceWorker';
 import { GlobalStyles, theme } from './styles';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <div>Hi</div>
+    <Content data-cy="content">
+      <Title data-cy="title">Soduku</Title>
+    </Content>
   </ThemeProvider>,
   document.getElementById('root')
 );
