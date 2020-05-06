247346b14d2a75034e2427276c953a7944eaf06e
sotomaque <sotomaque@gmail.com>
2020-05-06 01:41:56 -0700

    added core folder, moved service worker in there, set up redux in configure-store subfolder of core, implemented configureStore in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 
 import { Card, Content, Grid, Title } from 'components';
-import * as serviceWorker from 'serviceWorker';
+import { configureStore, unregister } from 'core';
 import { GlobalStyles, theme } from 'styles';
+
+const store = configureStore();
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <Content data-cy="content">
-      <Title data-cy="title">Soduku</Title>
-      <Card data-cy="card">
-        <Grid />
-      </Card>
-    </Content>
+    <Provider store={store}>
+      <Content data-cy="content">
+        <Title data-cy="title">Soduku</Title>
+        <Card data-cy="card">
+          <Grid />
+        </Card>
+      </Content>
+    </Provider>
   </ThemeProvider>,
   document.getElementById('root')
 );
 
-serviceWorker.unregister();
+unregister();
