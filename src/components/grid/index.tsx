0a9608a7c5a94828264151c0c4d0288c9f4e7ad3
sotomaque <sotomaque@gmail.com>
2020-05-06 02:24:01 -0700

    added react-moustrap for keybindings

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,4 +1,5 @@
 import React, { Children, FC, useCallback, useEffect } from 'react';
+import useMousetrap from 'react-hook-mousetrap';
 import { useDispatch } from 'react-redux';
 import { AnyAction, Dispatch } from 'redux';
 
@@ -6,6 +7,7 @@ import { createGrid } from 'reducers';
 
 import Block from './block';
 import { Container, Row } from './styles';
+import { INDEX } from 'typings';
 
 const Grid: FC = () => {
   const dispatch = useDispatch<Dispatch<AnyAction>>();
@@ -14,7 +16,28 @@ const Grid: FC = () => {
 
   useEffect(() => {
     create();
-  }, [create])
+  }, [create]);
+
+  function moveDown() {
+    console.log('down')
+  }
+
+  function moveUp() {
+    console.log('up')
+  }
+
+  function moveLeft() {
+    console.log('left')
+  }
+
+  function moveRight() {
+    console.log('right')
+  }
+
+  useMousetrap('down', moveDown)
+  useMousetrap('up', moveUp)
+  useMousetrap('left', moveLeft)
+  useMousetrap('right', moveRight)
 
   return (
     <Container data-cy="grid-container">
@@ -22,7 +45,12 @@ const Grid: FC = () => {
         [...Array(9)].map((_, rowIndex) => (
           <Row data-cy="grid-row-container">
             {Children.toArray(
-              [...Array(9)].map((_, colIndex) => <Block colIndex={colIndex} rowIndex={rowIndex} />)
+              [...Array(9)].map((_, colIndex) => (
+                <Block
+                  colIndex={colIndex as INDEX} 
+                  rowIndex={rowIndex  as INDEX} 
+                />
+              ))
             )}
           </Row>
         ))
--- src/reducers/reducer.ts
+++ src/reducers/reducer.ts
@@ -16,6 +16,12 @@ function reducer(state = initialState, action: AnyAction): IReducer {
                 grid: createFullGrid()
             }
 
+        case types.SELECT_BLOCK: 
+            return {
+                ...state,
+                selectedBlock: action.coords
+            }
+
         default:
             return state
     }
