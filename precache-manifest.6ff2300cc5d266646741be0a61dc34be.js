fa4e2403e286c9faaf1f22941eb44afb0994e80f
sotomaque <sotomaque@gmail.com>
2020-05-06 16:28:47 -0700

    Updates

---
+++ precache-manifest.6ff2300cc5d266646741be0a61dc34be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d27ac65fbd39cdfb1a43d70e33425f7",
+    "url": "/Soduku-V2-React/index.html"
+  },
+  {
+    "revision": "8796a9f9a02fd26f243d",
+    "url": "/Soduku-V2-React/static/js/2.b2d5da3c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Soduku-V2-React/static/js/2.b2d5da3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78d2e0aa8f8fdbe5fa20",
+    "url": "/Soduku-V2-React/static/js/main.071639b6.chunk.js"
+  },
+  {
+    "revision": "f020c5cde7cc89757d93",
+    "url": "/Soduku-V2-React/static/js/runtime-main.8b9f46ce.js"
+  }
+]);
