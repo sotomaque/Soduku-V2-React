0fb694fc15dd20a98eb511da3a16723bef809fce
sotomaque <sotomaque@gmail.com>
2020-05-06 21:50:57 -0700

    changed default difficulty

--- src/reducers/reducer.ts
+++ src/reducers/reducer.ts
@@ -18,7 +18,7 @@ function reducer(state = initialState, action: AnyAction): IReducer {
 
             const solvedGrid = createFullGrid();
             const gridCopy = copyGrid(solvedGrid);
-            const challengeGrid = removeNumbers(gridCopy, 1);
+            const challengeGrid = removeNumbers(gridCopy, 8);
 
             const workingGrid = copyGrid(challengeGrid);
 
